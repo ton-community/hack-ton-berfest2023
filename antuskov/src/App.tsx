ecde2159a0c2238bf12d87b8e862836317fc3594
sun-rhythms <anton.antuskov@yandex.ru>
2023-10-10 20:21:28 +0300

    workshop

---
+++ antuskov/src/App.tsx
@@ -0,0 +1,61 @@
+import './App.css';
+import styled from 'styled-components';
+import { Button, FlexBoxCol, FlexBoxRow } from './components/styled/styled';
+import { CHAIN, TonConnectButton } from '@tonconnect/ui-react';
+import { useTonConnect } from './hooks/useTonConnect';
+import '@twa-dev/sdk';
+import { BusinessCard } from './components/BusinessCard';
+import logo from './assets/hack-ton-berfest.jpg';
+import { useMemo } from 'react';
+
+const StyledApp = styled.div`
+  background-color: #12172c;
+  color: white;
+
+  @media (prefers-color-scheme: dark) {
+    background-color: #222;
+    color: white;
+  }
+  min-height: 100vh;
+  padding: 20px 20px;
+`;
+
+const AppContainer = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+const Logo = styled.div`
+  height: 48px;
+`;
+function App() {
+  const { network } = useTonConnect();
+
+  const currentNetwork = useMemo(() => {
+    switch (network) {
+      case CHAIN.MAINNET:
+        return 'mainnet';
+      case CHAIN.TESTNET:
+        return 'testnet';
+      default:
+        return 'N/A';
+    }
+  }, [network]);
+
+  return (
+    <StyledApp>
+      <AppContainer className={'container'}>
+        <FlexBoxCol>
+          <FlexBoxRow>
+            <img src={logo} height={48} />
+            <TonConnectButton />
+            <Button>{currentNetwork}</Button>
+          </FlexBoxRow>
+          <BusinessCard />
+        </FlexBoxCol>
+      </AppContainer>
+    </StyledApp>
+  );
+}
+
+export default App;
