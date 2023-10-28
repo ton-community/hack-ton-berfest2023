72a518ad2e7e24068954f6c0c412ba39192d21b7
heyhiru <hirentimbadiya74@gmail.com>
2023-10-28 18:14:59 +0530

    completed challenge
    Signed-off-by: heyhiru <hirentimbadiya74@gmail.com>

---
+++ hiren-timbadiya/src/hooks/useBusinessCardContract.ts
@@ -0,0 +1,80 @@
+import { Address, OpenedContract, toNano } from "ton-core";
+import { BusinessCard, Like } from "../wrappers/tact_BusinessCard";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { useTonClient } from "./useTonClient"
+import { useTonConnect } from "./useTonConnect";
+import { useEffect, useState } from "react";
+import { log } from "console";
+
+type UserInfo = {
+    name: string,
+    profession : string,
+    bio : string
+}
+
+export const useBusinessCardContract = () => {
+    const { client } = useTonClient();
+    const { sender } = useTonConnect();
+
+    const [likes, setLikes] = useState<number | null>();
+    const [userInfo, setUserInfo] = useState<UserInfo | null>();
+
+    const businessCardContract = useAsyncInitialize(async () => {
+        if (!client) return;
+
+        const contract = BusinessCard.fromAddress(
+            Address.parse("EQCM3b63cele_wx64hUJecFvmYA-xHbU4O0lyj3AJxqcLVEe")
+        );
+
+        return client.open(contract);
+    }, [client]);
+
+    const getLikes = async () => {
+        if (!businessCardContract) return;
+
+        const likes = await businessCardContract.getLikes();
+        setLikes(Number(likes));
+    }
+
+
+    const getUserInfo = async () => {
+        if(!businessCardContract) return;
+
+        setUserInfo(null);
+
+        const userInfo = await businessCardContract.getInfo();
+        setUserInfo({
+            name : userInfo.name,
+            profession : userInfo.profesion,
+            bio : userInfo.bio
+        })
+    }
+
+    const sendLike = async () => {
+        if(!businessCardContract) return;
+
+        const message : Like = {
+            $$type: "Like"
+        }
+
+        await businessCardContract.send(
+            sender,
+            {
+                value : toNano("0.01")
+            },
+            message
+        );
+
+    }
+
+    useEffect(() => {
+        getLikes().catch(console.log);
+        getUserInfo().catch(console.log);
+    } , [businessCardContract]);
+
+    return{
+        likes,
+        userInfo,
+        sendLike
+    }
+}
