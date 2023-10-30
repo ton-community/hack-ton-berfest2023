ef33be7776ca5abbd9ed3e8415b500338f92a676
Prince Changani <princechangani56385638@gmail.com>
2023-10-30 10:11:13 +0530

    challenge completed by implementing a function for the features like sendLike, getLike and getUserInfo
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

---
+++ prince-changani/src/hooks/useBusinessCardContract.ts
@@ -0,0 +1,81 @@
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { useTonClient } from "./useTonClient";
+import { useTonConnect } from "./useTonConnect";
+import { BusinessCard, Like } from "../wrappers/tact_BusinessCard";
+import { Address, OpenedContract, toNano } from "ton-core";
+import { useEffect, useState } from "react";
+import {log} from "console";
+
+type UserInfo = {
+    name: string,
+    profession: string,
+    bio: string
+}
+
+export function useBusinessCardContract(){
+    const {client} = useTonClient()
+    const {sender} = useTonConnect()
+
+    const [likes, setLikes] = useState<number | null>()
+    const [UserInfo, setUserInfo] = useState<UserInfo | null>()
+
+    const businessCardContract = useAsyncInitialize(async()=>{
+        if(!client) return;
+
+        const contract = BusinessCard.fromAddress(
+            Address.parse("EQCM3b63cele_wx64hUJecFvmYA-xHbU4O0lyj3AJxqcLVEe")
+        );
+
+        return client.open(contract)
+    }, [client])
+
+    async function getLikes()
+    {
+        if(!businessCardContract) return 
+
+        setLikes(null)  
+        const likes = await businessCardContract.getLikes()
+        setLikes(Number(likes))
+    }
+
+    async function getUserInfo()
+    {
+        if(!businessCardContract) return
+
+        setUserInfo(null)
+        const userInfo = await businessCardContract.getInfo()
+        setUserInfo({
+            name: userInfo.name,
+            profession: userInfo.profesion,
+            bio: userInfo.bio
+        })
+    }
+
+    async function sendLike()
+    {
+        if(!businessCardContract) return;
+
+        const message : Like = {
+            $$type: "Like"
+        }
+
+        await businessCardContract.send(
+            sender,
+            {
+                value: toNano("0.01")
+            },
+            message
+        )
+    }
+
+    useEffect(()=>{
+        getLikes().catch(console.log);
+        getUserInfo().catch(console.log);
+    }, [businessCardContract]);
+
+    return {
+        likes,
+        UserInfo,
+        sendLike
+    }
+}
